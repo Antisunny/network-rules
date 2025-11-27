83604036d6f06ba36ceb014269aebd0109c9a77a
RyanLincs <ryan.lincs@outlook.com>
2025-11-27 20:22:23 +0800

    rewrite notability

---
+++ quantumult-x/scripts/notability.js
@@ -0,0 +1,33 @@
+// Notability
+// copied from https://github.com/chxm1023/Rewrite/blob/main/Notability.js
+// also https://github.com/89996462/Quantumult-X/blob/main/ycdz/notability1.js
+
+
+var content = JSON.parse($response.body);
+
+content = {
+    "data": {
+        "processAppleReceipt": {
+            "error": 0,
+            "subscription": {
+                "productId": "com.gingerlabs.Notability.premium_subscription",
+                "originalTransactionId": "570001184068302",
+                "tier": "premium",
+                "refundedDate": null,
+                "refundedReason": null,
+                "isInBillingRetryPeriod": false,
+                "expirationDate": "2099-09-09T09:09:09.000Z",
+                "gracePeriodExpiresAt": null,
+                "overDeviceLimit": false,
+                "expirationIntent": "CUSTOMER_CANCELLED",
+                "__typename": "AppStoreSubscription",
+                "user": null,
+                "status": "canceled",
+                "originalPurchaseDate": "2022-09-09T09:09:09.000Z"
+            },
+            "__typename": "SubscriptionResult"
+        }
+    }
+};
+
+$done({ body: JSON.stringify(ojbk) });
